0ae6c0479c40858ca75ee7363464ad1005c618ca
Pedro Martins <up201705008@fe.up.pt>
2020-11-24 19:42:51 +0000

    teste commit

--- Client/src/ClientStart.java
+++ Client/src/ClientStart.java
@@ -1,8 +1,13 @@
+import javax.swing.*;
+
 public class ClientStart {
     public static void main(String[] args) {
-        Client client;
-        client = new Client("176.79.140.197");	//local host
-        //client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        client.startRunning();
+//        Client client;
+//        client = new Client("176.79.140.197");	//local host
+//        client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        client.startRunning();
+
+        JFrame register = new JFrame("Register");
+
     }
 }
