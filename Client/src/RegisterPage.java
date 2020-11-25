923f0010e03cb37c238ae79fa189da9f86fb72a7
Pedro Martins <up201705008@fe.up.pt>
2020-11-25 23:02:57 +0000

    Refactor: Separate GUI from logic in Register

--- Client/src/RegisterPage.java
+++ Client/src/RegisterPage.java
@@ -4,13 +4,13 @@ import com.intellij.uiDesigner.core.GridLayoutManager;
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Arrays;
 
 public class RegisterPage {
     private JPanel panel1;
     private JTextField userField;
+
+
     private JPasswordField pwdField;
     private JLabel titleLabel;
     private JLabel userLabel;
@@ -19,44 +19,63 @@ public class RegisterPage {
     private JLabel emailLabel;
     private JLabel repeatPwdLabel;
     private JPasswordField repeatPwdField;
-    private JButton registerButton;
+
+
     private JButton leaveButton;
+    JFrame frame;
 
     public RegisterPage() {
-        JFrame frame = new JFrame("Register");
+        frame = new JFrame("Register");
         frame.add(panel1);
         frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ActionListener handler = new RegisterLogic(this);
+        registerButton.addActionListener(handler);
+        leaveButton.addActionListener(handler);
+    }
 
-        registerButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                registerButton.setEnabled(false);
+    public void setRegisterButtonEnabled(boolean condition) {
+        registerButton.setEnabled(condition);
+    }
 
-                char pwd[] = pwdField.getPassword();
-                char repeatPwd[] = repeatPwdField.getPassword();
+    public JTextField getUserField() {
+        return userField;
+    }
 
-                if (!Arrays.equals(pwd, repeatPwd)) {
-                    JOptionPane.showMessageDialog(frame, "Passwords don't match.");
-                    pwdField.setText("");
-                    repeatPwdField.setText("");
-                    registerButton.setEnabled(true);
-                }
-                else {
-                    registerButton.setEnabled(false);
-                    emailField.getText();
-                    userField.getText();
-                }
+    public JPasswordField getPwdField() {
+        return pwdField;
+    }
 
-            }
-        });
-        leaveButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                frame.dispose();
-            }
-        });
+    public JTextField getEmailField() {
+        return emailField;
+    }
+
+    public JPasswordField getRepeatPwdField() {
+        return repeatPwdField;
+    }
+
+    public void setPwdField_repeatPwdFieldEmpty() {
+        pwdField.setText("");
+        repeatPwdField.setText("");
+    }
+
+    public void showMessage(String msg) {
+        JOptionPane.showMessageDialog(frame, msg);
+    }
+
+    public void disposeFrame() {
+        frame.dispose();
+    }
+
+    public JButton getRegisterButton() {
+        return registerButton;
+    }
+
+    private JButton registerButton;
+
+    public JButton getLeaveButton() {
+        return leaveButton;
     }
 
     private void createUIComponents() {
@@ -121,6 +140,7 @@ public class RegisterPage {
         leaveButton = new JButton();
         leaveButton.setBackground(new Color(-2686936));
         leaveButton.setForeground(new Color(-1));
+        leaveButton.setIcon(new ImageIcon(getClass().getResource("/img/back-arrow.png")));
         leaveButton.setText("Leave");
         panel1.add(leaveButton, new GridConstraints(5, 1, 1, 1, GridConstraints.ANCHOR_EAST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         userLabel.setLabelFor(userField);
