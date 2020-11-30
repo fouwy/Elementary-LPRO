d5cac9a489fe56ff00fd2a15d7a95910df2a86c3
Pedro Martins <up201705008@fe.up.pt>
2020-11-30 15:08:59 +0000

    Add JUnit Tests for Database

--- Server/src/SqliteHelper.java
+++ Server/src/SqliteHelper.java
@@ -4,14 +4,19 @@ import java.sql.SQLException;
 
 public class SqliteHelper {
     private static Connection c = null;
+    private static String pathToDB = "C:/sqlite3/jdbc-test/accounts.db";
 
     //Only have one connection for for everyone accessing
     //TODO: When creating more threads for server maybe have to change to synchronized method
     public static Connection getConnection() throws SQLException {
         if(c == null){
             System.out.println("Opening new connection to Database");
-            c = DriverManager.getConnection("jdbc:sqlite:C:/sqlite3/jdbc-test/accounts.db");
+            c = DriverManager.getConnection("jdbc:sqlite:"+ pathToDB);
         }
         return c;
+    }
+
+    public static void setDatabasePath(String pathToDB) {
+        SqliteHelper.pathToDB = pathToDB;
     }
 }
