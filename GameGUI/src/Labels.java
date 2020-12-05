d2e2147172ee3ffc448710b082146874948a1bd1
Daniel <up201705410@fe.up.pt>
2020-12-05 00:38:10 +0000

    initial commit

---
+++ GameGUI/src/Labels.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Labels extends JLabel{
+	
+	Labels( int x, int y, int width, int height){
+		this.setHorizontalTextPosition(JLabel.CENTER);
+		this.setBounds(x, y, width, height);
+	}
+		
+	
+}
